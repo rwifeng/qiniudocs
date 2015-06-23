822d8fbb853059e84ff805f9a65290fc7feed6ac
rwifeng <rw.ifeng@gmail.com>
2015-06-23 18:29:49 +0800

    add callback page

---
+++ demo/simpleuploader/fileinfo.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <style>
+            div {
+                width:50%;
+                margin:100px 25% auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+                <?php 
+                    $ret = base64_decode($_GET['upload_ret']);
+                    $cbody = json_decode($ret, true);
+                    $dn = 'http://77fxsr.com2.z0.glb.qiniucdn.com/';  
+                    error_log(print_r($cbody, true));
+                    $url = $dn . $cbody['fname'];
+
+                    $stat_ = file_get_contents($url . '?stat');
+                    $stat = json_decode($stat_, true);
+                    $mtype = $stat['mimeType']; 
+                    $isImage = substr($mtype, 0, 6) == 'image/'
+                ?>
+                <p>上传文件的外链：<a href=<?=$url?>><?=$url?></a></p> 
+                <? if($isImage): >
+                    <img src=<?=$url?> width="700" height="400" alt=""></img>
+                <? endif;?>
+                <p><a href="http://127.0.0.1/demo/simpleuploader/index.html">返回</a></p> 
+        </div> 
+    </body>
+</html>
